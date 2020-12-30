5d5515328b60edafd75b24146906aaaccd1ddd65
ridwannulloh <nulloh.ridwan0320@gmail.com>
2020-12-30 21:22:12 +0700

    Finish creating own module in python

---
+++ geometri/persegipanjang.py
@@ -0,0 +1,5 @@
+def info():
+    return 'Modul menghitung rumus-rumus tentang persegi panjang'
+
+def hitung_luas_persegi_panjang(p, l):
+    return p * l
---
+++ use_geometry.py
@@ -0,0 +1,8 @@
+from geometri.persegipanjang import hitung_luas_persegi_panjang, info as info_persegipanjang
+from geometri.segitiga import hitung_luas_segitiga, info as info_segitiga
+
+print(info_segitiga())
+print(f'hitung_luas_segitiga {hitung_luas_segitiga(10, 3)}')
+
+print(info_persegipanjang())
+print(f'hitung_luas_persegi_panjang {hitung_luas_persegi_panjang(10, 3)}')
