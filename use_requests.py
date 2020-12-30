021998874a243b91dd45e1bf4dbb11818ef80de3
ridwannulloh <nulloh.ridwan0320@gmail.com>
2020-12-30 21:48:40 +0700

    using eksternal package requests and generating requirements.txt

---
+++ use_requests.py
@@ -0,0 +1,17 @@
+import requests
+
+url = 'https://detik.com'
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f'Success! Response = {response.status_code}')
+        print(f'Content {response.text}')
+    else:
+        print(f'Woops, ada kesalahan requests {response.status_code}')
+except Exception as e:
+    print(f'There is an error {e}')
+print('Program Standard')
+
+
+
+
